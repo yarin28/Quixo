84c232b99b64fc237e09326bd19f5211706f1f84
Yarin <yarin.greenfeld@gmail.com>
2021-12-20 19:24:02 +0200

    unused imports todo

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+//TODO: all the imports are not used should be gone
 namespace Quixo
 {
     /// <summary>

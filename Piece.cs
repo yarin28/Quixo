285acaf27cd42b4b306384f6c77f11ec42175211
Yarin <yarin.greenfeld@gmail.com>
2021-12-20 17:44:08 +0200

    Piece class for every piece

---
+++ Piece.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+namespace Quixo
+{
+    internal class Piece 
+    {
+        Piece()
+        {
+            Rectangle rec = new Rectangle
+                {
+                    Width = Consts.PieceSize,
+                    Height = Consts.PieceSize,
+                    Fill = System.Windows.Media.Brushes.Brown  
+            };
+        }
+    }
+}

fb160367966b03384c005f4ca8871c0792ec3833
Yarin <yarin.greenfeld@gmail.com>
2021-12-20 17:43:11 +0200

    added consts class make it simpler to have consts

---
+++ Consts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quixo
+{
+    internal class Consts
+    {
+        public static  int PieceSize = 400 / 5;
+    }
+}

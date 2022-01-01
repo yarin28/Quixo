013962b85d63f6abaa095532423618269f1abf24
Yarin <yarin.greenfeld@gmail.com>
2022-01-01 23:38:42 +0200

    added Board class

---
+++ Board.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quixo
+{
+    //public sealed  class Board
+    public class Board
+    {
+		public const int Dimension = 5;
+
+		private Player winningPlayer = Player.None;
+		private Player currentPlayer = Player.X;
+		private long pieces;
+		private MoveCollection moveHistory = new MoveCollection();
+		public Board()
+        {
+			this.pieces = 0;
+
+        }
+		public byte GetDimension()
+        {
+			return (byte)Dimension;
+        }
+	}
+}

729d1d0ed4321782d55ab9f96d1e5399ed856c37
Yarin <yarin.greenfeld@gmail.com>
2022-01-01 23:38:03 +0200

    Added the tests poject to the assembly
    i had to do that to make the tests and the actual project seperate

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,5 +1,5 @@
+using System.Runtime.CompilerServices;
 using System.Windows;
-
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
                                      //(used if a resource is not found in the page,
@@ -8,3 +8,4 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+[assembly: InternalsVisibleTo("Quixo.Tests")]

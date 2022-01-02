f7d5cada53b37a6a94a0bd873a6bf9b53314656d
Yarin <yarin.greenfeld@gmail.com>
2022-01-02 22:31:39 +0200

    added the logic class

---
+++ Logic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quixo
+{
+    internal class Logic
+    {
+        private long[] wonBorad;
+        public static Boolean ValidateMove(Point start,Point end)
+        {
+          
+            return false;
+        }
+    }
+}

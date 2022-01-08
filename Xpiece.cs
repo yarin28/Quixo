c2ffa247bf60863436fbc38969fbb2c25d74105b
Yarin <yarin.greenfeld@gmail.com>
2022-01-08 18:46:10 +0200

    added is won byte array

---
+++ Xpiece.cs
@@ -0,0 +1,14 @@
+﻿namespace Quixo
+{
+    //probably i dont need this but this si a useful idea
+    internal class Xpiece
+    {
+        private int x;
+        private int y;
+        public  Xpiece(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}

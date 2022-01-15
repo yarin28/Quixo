03a8507de4e453c6f828bbfbb3dee6a4bcfca71f
Yarin <yarin.greenfeld@gmail.com>
2022-01-15 18:23:54 +0200

    aded GetPiece

--- Board.cs
+++ Board.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +24,25 @@ namespace Quixo
         {
 			return (byte)Dimension;
         }
+		public Player GetPiece(Point position)
+		{
+			if (position.X < 0 || position.X > (Board.Dimension) ||
+				position.Y < 0 || position.Y > (Board.Dimension))
+			{
+				throw new IndexOutOfRangeException($"Point {position.ToString()} is out of range.");
+			}
+
+			int shiftOut = this.GetShiftOut(position.X, position.Y);
+			if ((this.pieces & (1L << shiftOut)) ==(1L << shiftOut))
+			{
+				return Player.X;
+			}
+			else if ((this.pieces & (1L >> (shiftOut + 32))) == (1L>>(shiftOut + 32)))
+                {
+				return Player.O;
+            }
+			return (Player)Player.None;
+		}
+
 	}
 }

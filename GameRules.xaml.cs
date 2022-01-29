5718759e0495cf1828f2c3a865999e13b91691c3
Yarin <yarin.greenfeld@gmail.com>
2022-01-30 00:03:45 +0200

    added the cs

---
+++ GameRules.xaml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Quixo
+{
+    /// <summary>
+    /// Interaction logic for GameRules.xaml
+    /// </summary>
+    public partial class GameRules : Window
+    {
+        public GameRules()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+        public string MyPic
+        {
+            get { return @"https://img.fruugo.com/product/3/95/143031953_max.jpg"; }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}

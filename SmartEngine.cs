a6da8dd9e30c1872799a444195fa14b2579d9471
Yarin <yarin.greenfeld@gmail.com>
2022-04-06 14:11:09 +0300

    added EvaluateHorizontalLines

--- SmartEngine.cs
+++ SmartEngine.cs
@@ -15,5 +15,88 @@ namespace Quixo
         private const int WinningLine = int.MaxValue;
 		private System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
 
+        //the Evaluate function must reword 2 things
+		//1- the number of pieces of the current player
+		//2- the connections between the pieces of the current player
+		//TODO: this will probably have to change.
+	private int UpdateContinuation(int currentContinuationValue) =>
+			(currentContinuationValue ^ 2) * 4;
+
+		private int EvaluateHorizontalLines(Board board, int evaluation)
+		{
+			var horizontalEvaluation = evaluation;
+			bool hasWinningLine = false, hasLosingLine = false;
+
+			for(var y = 0; y < Board.Dimension; y++)
+			{
+				var lineState = board.GetPiece(0, y);
+
+				if(lineState == board.CurrentPlayer)
+				{
+					horizontalEvaluation++;
+				}
+				else if(lineState != Player.None)
+				{
+					horizontalEvaluation--;
+				}
+
+				var continuationFactor = 1;
+
+				for(var x = 1; x < Board.Dimension; x++)
+				{
+					var currentPiece = board.GetPiece(x, y);
+
+					if(currentPiece == board.CurrentPlayer)
+					{
+						horizontalEvaluation++;
+					}
+					else if(currentPiece != Player.None)
+					{
+						horizontalEvaluation--;
+					}
+
+					if(currentPiece == board.GetPiece(x - 1, y))
+					{
+						continuationFactor = this.UpdateContinuation(continuationFactor);
+
+						if(currentPiece == board.CurrentPlayer)
+						{
+							horizontalEvaluation += continuationFactor;
+						}
+						else if(currentPiece != Player.None)
+						{
+							horizontalEvaluation -= continuationFactor;
+						}
+					}
+					else
+					{
+						lineState = Player.None;
+						continuationFactor = 1;
+					}
+				}
+
+				if(lineState == board.CurrentPlayer)
+				{
+					hasWinningLine = true;
+					break;
+				}
+				else if(lineState != Player.None)
+				{
+					hasLosingLine = true;
+					break;
+				}
+			}
+
+			if(hasWinningLine == true && hasLosingLine == false)
+			{
+				horizontalEvaluation = WinningLine;
+			}
+			else if(hasLosingLine == true)
+			{
+				horizontalEvaluation = LosingLine;
+			}
+
+			return horizontalEvaluation;
+		}
     }
 }

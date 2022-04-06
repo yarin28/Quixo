267eed780d0f1d8529efae08cbaf1e286cd2d76a
Yarin <yarin.greenfeld@gmail.com>
2022-04-06 14:11:34 +0300

    added EvaluateVerticalLines

--- SmartEngine.cs
+++ SmartEngine.cs
@@ -98,5 +98,83 @@ namespace Quixo
 
 			return horizontalEvaluation;
 		}
+
+		private int EvaluateVerticalLines(Board board, int evaluation)
+		{
+			var verticalEvaluation = evaluation;
+			bool hasWinningLine = false, hasLosingLine = false;
+
+			for(var x = 0; x < Board.Dimension; x++)
+			{
+				var lineState = board.GetPiece(x, 0);
+
+				if(lineState == board.CurrentPlayer)
+				{
+					verticalEvaluation++;
+				}
+				else if(lineState != Player.None)
+				{
+					verticalEvaluation--;
+				}
+
+				var continuationFactor = 1;
+
+				for(var y = 1; y < Board.Dimension; y++)
+				{
+					var currentPiece = board.GetPiece(x, y);
+
+					if(currentPiece == board.CurrentPlayer)
+					{
+						verticalEvaluation++;
+					}
+					else if(currentPiece != Player.None)
+					{
+						verticalEvaluation--;
+					}
+
+					if(currentPiece == board.GetPiece(x, y - 1))
+					{
+						continuationFactor = this.UpdateContinuation(continuationFactor);
+
+						if(currentPiece == board.CurrentPlayer)
+						{
+							verticalEvaluation += continuationFactor;
+						}
+						else if(currentPiece != Player.None)
+						{
+							verticalEvaluation -= continuationFactor;
+						}
+					}
+					else
+					{
+						lineState = Player.None;
+						continuationFactor = 1;
+					}
+				}
+
+				if(lineState == board.CurrentPlayer)
+				{
+					hasWinningLine = true;
+					break;
+				}
+				else if(lineState != Player.None)
+				{
+					hasLosingLine = true;
+					break;
+				}
+			}
+
+			if(hasWinningLine == true && hasLosingLine == false)
+			{
+				verticalEvaluation = WinningLine;
+			}
+			else if(hasLosingLine == true)
+			{
+				verticalEvaluation = LosingLine;
+			}
+
+			return verticalEvaluation;
+		}
+
     }
 }

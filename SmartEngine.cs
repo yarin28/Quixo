9fed6da1a9c65ae47285b141a143caa2e1d15fbb
Yarin <yarin.greenfeld@gmail.com>
2022-04-06 14:12:03 +0300

    added EvaluateDiagonalLines

--- SmartEngine.cs
+++ SmartEngine.cs
@@ -176,5 +176,124 @@ namespace Quixo
 			return verticalEvaluation;
 		}
 
+		private int EvaluateDiagonalLines(Board board, int evaluation)
+		{
+			var diagonalEvaluation = evaluation;
+
+			var lineState = board.GetPiece(0, 0);
+
+			if(lineState == board.CurrentPlayer)
+			{
+				diagonalEvaluation++;
+			}
+			else if(lineState != Player.None)
+			{
+				diagonalEvaluation--;
+			}
+
+			var continuationFactor = 1;
+
+			for(var x = 1; x < Board.Dimension; x++)
+			{
+				var currentPiece = board.GetPiece(x, x);
+
+				if(currentPiece == board.CurrentPlayer)
+				{
+					diagonalEvaluation++;
+				}
+				else if(currentPiece != Player.None)
+				{
+					diagonalEvaluation--;
+				}
+
+				if(currentPiece == board.GetPiece(x - 1, x - 1))
+				{
+					continuationFactor = this.UpdateContinuation(continuationFactor);
+
+					if(currentPiece == board.CurrentPlayer)
+					{
+						diagonalEvaluation += continuationFactor;
+					}
+					else if(currentPiece != Player.None)
+					{
+						diagonalEvaluation -= continuationFactor;
+					}
+				}
+				else
+				{
+					lineState = Player.None;
+					continuationFactor = 1;
+				}
+			}
+
+			if(lineState != Player.None && lineState != board.CurrentPlayer)
+			{
+				diagonalEvaluation = LosingLine;
+			}
+			else
+			{
+				if(lineState == board.CurrentPlayer)
+				{
+					diagonalEvaluation = WinningLine;
+				}
+				else
+				{
+					lineState = board.GetPiece(0, Board.Dimension - 1);
+
+					if(lineState == board.CurrentPlayer)
+					{
+						diagonalEvaluation++;
+					}
+					else if(lineState != Player.None)
+					{
+						diagonalEvaluation--;
+					}
+
+					for(var x = 1; x < Board.Dimension; x++)
+					{
+						var currentPiece = board.GetPiece(x, Board.Dimension - 1 - x);
+
+						if(currentPiece == board.CurrentPlayer)
+						{
+							diagonalEvaluation++;
+						}
+						else if(currentPiece != Player.None)
+						{
+							diagonalEvaluation--;
+						}
+
+						if(currentPiece == board.GetPiece(x - 1, Board.Dimension - x))
+						{
+							continuationFactor = this.UpdateContinuation(continuationFactor);
+
+							if(currentPiece == board.CurrentPlayer)
+							{
+								diagonalEvaluation += continuationFactor;
+							}
+							else if(currentPiece != Player.None)
+							{
+								diagonalEvaluation -= continuationFactor;
+							}
+						}
+						else
+						{
+							lineState = Player.None;
+							continuationFactor = 1;
+						}
+					}
+
+					if(lineState == board.CurrentPlayer)
+					{
+						diagonalEvaluation = WinningLine;
+					}
+					else if(lineState != Player.None)
+					{
+						diagonalEvaluation = LosingLine;
+					}
+				}
+			}
+
+			return diagonalEvaluation;
+		}
     }
 }

d1b74b49f3a000701c85527000cf60b7a48edc57
Yarin <yarin.greenfeld@gmail.com>
2022-04-06 14:07:56 +0300

    added the smart engine class

---
+++ SmartEngine.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Quixo
+{
+    public class SmartEngine
+    {
+        private const int DepthLimit = 4;
+        private const int LosingLine = int.MinValue;
+        private const int WinningLine = int.MaxValue;
+		private System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+
+    }
+}

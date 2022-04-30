f65c5604777503b5a83b02f03f77515f580634e9
Yarin <yarin.greenfeld@gmail.com>
2022-05-01 01:47:34 +0300

    starting new event with move

--- Move.cs
+++ Move.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.Drawing;
 
 namespace Quixo
@@ -7,21 +8,21 @@ namespace Quixo
     /// This class represents a move within a Quixo game.
     /// </summary>
     [Serializable]
-    public sealed class Move
+    public sealed class Move:INotifyPropertyChanged
     {
         private readonly Player player = Player.None;
+        public Player Player { get { return player; } }
         private Point source = Point.Empty;
+        public Point Source { get { return source; } }
         private Point destination = Point.Empty;
+        public Point Destination { get { return destination; } }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         public Move(Player player, Point source, Point destination) =>
             (this.player, this.source, this.destination) = (player, source, destination);
 
         public string Print() => $"Player {this.player}: {this.source} to {this.destination}";
 
-        public Point Destination => this.destination;
-
-        public Player Player => this.player;
-
-        public Point Source => this.source;
     }
 }

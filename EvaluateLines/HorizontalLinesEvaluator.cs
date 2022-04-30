b68cd103382ac3eaaae099aa859ed6000aceeba9
Yarin <yarin.greenfeld@gmail.com>
2022-05-01 01:27:08 +0300

    gave horizontal evaluator its own file

---
+++ EvaluateLines/HorizontalLinesEvaluator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quixo.EvaluateLines
+{
+    internal class HorizontalLinesEvaluator : LinesEvaluator
+    {
+        public override int EvaluateLines(Board board, int evaluation)
+		{
+			var horizontalEvaluation = evaluation;
+			bool hasWinningLine = false, hasLosingLine = false;
+
+			for(var y = 0; y < Board.Dimension; y++)
+			{
+				var lineState = board.GetPiece(0, y);
+
+				if(lineState == board.CurrentPlayer)
+				{
+					horizontalEvaluation++;
+				}
+				else if(lineState != Player.None)
+				{
+					horizontalEvaluation--;
+				}
+
+				var continuationFactor = 1;
+
+				for(var x = 1; x < Board.Dimension; x++)
+				{
+					var currentPiece = board.GetPiece(x, y);
+
+					if(currentPiece == board.CurrentPlayer)
+					{
+						horizontalEvaluation++;
+					}
+					else if(currentPiece != Player.None)
+					{
+						horizontalEvaluation--;
+					}
+
+					if(currentPiece == board.GetPiece(x - 1, y))
+					{
+						continuationFactor = this.UpdateContinuation(continuationFactor);
+
+						if(currentPiece == board.CurrentPlayer)
+						{
+							horizontalEvaluation += continuationFactor;
+						}
+						else if(currentPiece != Player.None)
+						{
+							horizontalEvaluation -= continuationFactor;
+						}
+					}
+					else
+					{
+						lineState = Player.None;
+						continuationFactor = 1;
+					}
+				}
+
+				if(lineState == board.CurrentPlayer)
+				{
+					hasWinningLine = true;
+					break;
+				}
+				else if(lineState != Player.None)
+				{
+					hasLosingLine = true;
+					break;
+				}
+			}
+
+			if(hasWinningLine == true && hasLosingLine == false)
+			{
+				horizontalEvaluation = WinningLine;
+			}
+			else if(hasLosingLine == true)
+			{
+				horizontalEvaluation = LosingLine;
+			}
+
+			return horizontalEvaluation;
+		}
+
+    }
+}

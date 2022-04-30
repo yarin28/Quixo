093ed2f6ca963eba1659787740f7ee0d6a4c5678
Yarin <yarin.greenfeld@gmail.com>
2022-05-01 01:23:49 +0300

    added the evaluate consts

--- EvaluateLines/LinesEvaluator.cs
+++ EvaluateLines/LinesEvaluator.cs
@@ -11,3 +11,5 @@ namespace Quixo
         protected int UpdateContinuation(int currentContinuationValue) =>
            
 			(currentContinuationValue ^ 2) * 4;
+        protected const int LosingLine = int.MinValue;
+        protected const int WinningLine = int.MaxValue;

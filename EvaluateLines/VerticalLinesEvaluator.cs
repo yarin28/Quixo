1b937af4b962908a36e9775eebc4da37a0d05838
Yarin <yarin.greenfeld@gmail.com>
2022-05-01 01:26:21 +0300

    moved the verticalLinesEvaluator to his class

---
+++ EvaluateLines/VerticalLinesEvaluator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quixo.EvaluateLines
+{
+    internal class VerticalLinesEvaluator : LinesEvaluator
+    {
+        public override int EvaluateLines(Board board, int evaluation)
+		{
+			var verticalEvaluation = evaluation;
+			bool hasWinningLine = false, hasLosingLine = false;
+
+			for(var x = 0; x < Board.Dimension; x++)
+			{
+				var lineState = board.GetPiece(x, 0);
+
+				if(lineState == board.CurrentPlayer)
+				{
+					verticalEvaluation++;
+				}
+				else if(lineState != Player.None)
+				{
+					verticalEvaluation--;
+				}
+
+				var continuationFactor = 1;
+
+				for(var y = 1; y < Board.Dimension; y++)
+				{
+					var currentPiece = board.GetPiece(x, y);
+
+					if(currentPiece == board.CurrentPlayer)
+					{
+						verticalEvaluation++;
+					}
+					else if(currentPiece != Player.None)
+					{
+						verticalEvaluation--;
+					}
+
+					if(currentPiece == board.GetPiece(x, y - 1))
+					{
+						continuationFactor = this.UpdateContinuation(continuationFactor);
+
+						if(currentPiece == board.CurrentPlayer)
+						{
+							verticalEvaluation += continuationFactor;
+						}
+						else if(currentPiece != Player.None)
+						{
+							verticalEvaluation -= continuationFactor;
+						}
+					}
+					else
+					{
+						lineState = Player.None;
+						continuationFactor = 1;
+					}
+				}
+
+				if(lineState == board.CurrentPlayer)
+				{
+					hasWinningLine = true;
+					break;
+				}
+				else if(lineState != Player.None)
+				{
+					hasLosingLine = true;
+					break;
+				}
+			}
+
+			if(hasWinningLine == true && hasLosingLine == false)
+			{
+				verticalEvaluation = WinningLine;
+			}
+			else if(hasLosingLine == true)
+			{
+				verticalEvaluation = LosingLine;
+			}
+
+			return verticalEvaluation;
+		}
+
+    }
+}

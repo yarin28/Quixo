44a2b46fe0fc02b86fa0468f3a8cd57f7f99cc78
Yarin <yarin.greenfeld@gmail.com>
2022-05-01 01:36:20 +0300

    improved everything!+ formating

--- SmartEngine.cs
+++ SmartEngine.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
 ways to improve the smart engine:
 [ ]: add more threads to calculate. not possible currently
 		 with the minimax algorithm because it has to compare everything
@@ -8,7 +8,8 @@ ways to improve the smart engine:
 		will boost the performance.
 */
 using System.Linq;
-using System.Diagnostics;
+using Quixo.EvaluateLines;
+using System;
 
 namespace Quixo
 {
@@ -21,81 +22,59 @@ namespace Quixo
             new HorizontalLinesEvaluator(),
             new VerticalLinesEvaluator(),
             new DiagonalLinesEvaluator()};
+        public Move GenerateMove(Board board)
         {
-			watch.Reset();
-			watch.Start();
-			int bestValue = int.MinValue;
-            Move? generatedMove = null ;
-            foreach (var source in board.GetValidSourcePieces())
-            {
-                foreach (var destination in board.GetValidDestinationPieces(source))
-                {
-                    var nextMoveBoard = ((Board)board.Clone());
-                    nextMoveBoard.MovePiece(source, destination);
-                    var possibleBestMove = this.MiniMaxWithAlphaBeta(nextMoveBoard, board.CurrentPlayer, false, 1, int.MinValue, int.MaxValue);
-                    if (possibleBestMove > bestValue || (possibleBestMove >= bestValue && generatedMove == null))
-                    {
-                        bestValue = possibleBestMove;
-                        generatedMove = new Move(board.CurrentPlayer, source, destination);
-                    }
+                    var (generatedMove, evaluation) = MiniMaxWithAlphaBeta(board, board.CurrentPlayer, true, 1,  int.MinValue, int.MaxValue);
 
-                }
-            }
-			watch.Stop();
-			Trace.WriteLine($"Execution Time:{watch.ElapsedMilliseconds} ms");
-			return generatedMove;
+            if (generatedMove == null)
+                throw new NullReferenceException("generatedMove was null");
+            return generatedMove;
         }
-		private int MiniMaxWithAlphaBeta(Board board, Player currentPlayer, bool isMaximizing, int depth, int alpha, int beta)
-		{
+        private (Move?,int) MiniMaxWithAlphaBeta(Board board, Player currentPlayer, bool isMaximizing, int depth,  int  alpha, int beta)
+        {
             int evaluation;
-            if (depth>=DepthLimit||board.WinningPlayer!=Player.None)
-			{
+            Move? nextMove = null;
+            if (depth >= DepthLimit || board.WinningPlayer != Player.None)
+            {
 
-				evaluation = this.Evaluate(board,currentPlayer);
-				if(board.CurrentPlayer!=Player.None&&board.CurrentPlayer!=currentPlayer)
-				{
-					evaluation*=-1;
-				}
-			else if(board.CurrentPlayer==Player.None&&board.WinningPlayer!=currentPlayer)
-				{
-					evaluation*=-1;
-				}
-			}
-			else
-			{
-				var nextEvaluation=0;
-				foreach(var source in board.GetValidSourcePieces())
-				{
-					foreach(var destination in board.GetValidDestinationPieces(source))
-					{
-						//Trace.WriteLine(String.Format("{0}{1}{2}",source.ToString(), destination.ToString(),depth.ToString()));
-						var nextMoveBoard = ((Board)board.Clone());
-						nextMoveBoard.MovePiece(source,destination);
-						var newDepth = depth;
-						nextEvaluation = this.MiniMaxWithAlphaBeta(nextMoveBoard,currentPlayer,!isMaximizing,++newDepth,alpha,beta);
-						if(alpha>beta)
-						{
-							break;
-						}
-						else if (isMaximizing==false&&nextEvaluation<beta)
-						{
-							beta = nextEvaluation;
-						}
-						else if (isMaximizing==true&&nextEvaluation>alpha)
-						{
-							alpha = nextEvaluation;
-						}
-					}
-					if(alpha>beta)
-					{
-						break;
-					}
-				}
-				evaluation = isMaximizing?alpha:beta;
+                evaluation = this.Evaluate(board, currentPlayer);
+                if (board.CurrentPlayer != Player.None && board.CurrentPlayer != currentPlayer)
+                    evaluation *= -1;
+                else if (board.CurrentPlayer == Player.None && board.WinningPlayer != currentPlayer)
+                    evaluation *= -1;
+            }
+            else
+            {
+                foreach (var source in board.GetValidSourcePieces())
+                {
+                    foreach (var destination in board.GetValidDestinationPieces(source))
+                    {
+                        var nextMoveBoard = board.Clone();
+                        nextMoveBoard.MovePiece(source, destination);
+                        var newDepth = depth;
+                        var (_, nextEvaluation) = this.MiniMaxWithAlphaBeta(nextMoveBoard, currentPlayer, !isMaximizing, ++newDepth, alpha, beta);
+                        if (alpha > beta)
+                            break;
+                        else if (isMaximizing == false && nextEvaluation < beta)
+                        {
+                            nextMove = new(board.CurrentPlayer, source, destination);
+                            beta = nextEvaluation;
+                        }
+                        else if (isMaximizing == true && nextEvaluation > alpha)
+                        {
+                            nextMove = new(board.CurrentPlayer, source, destination);
+                            alpha = nextEvaluation;
+                        }
+                    }
+                    if (alpha > beta)
+                        break;
+                }
+                evaluation = isMaximizing ? alpha : beta;
 
-			}
-			return evaluation;
-		}
+            }
+            //NOTE: if the nextMove is null someone has won.
+            return (nextMove, evaluation);
+        }
         #region Evaluating function
 
         public int Evaluate(Board board, Player currentPlayer)

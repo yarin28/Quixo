50eb2781bb916183b96527c2e2b3acb7a917faf6
Yarin <yarin.greenfeld@gmail.com>
2022-05-02 06:45:44 +0300

    moved printable move to own file

---
+++ PrintableMove.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quixo
+{
+        public class PrintableMove
+        {
+            public string player
+            { get; set; }
+            public string source
+            { get; set; }
+            public string destination
+            { get; set; }
+            public PrintableMove(Move mov)
+            {
+                this.player = mov.Player.ToString();
+                this.source = mov.Source.ToString();
+                this.destination = mov.Destination.ToString();
+            }
+
+            public PrintableMove(Player player, System.Drawing.Point source, System.Drawing.Point dest)
+            {
+                this.player = player.ToString();
+                this.source = source.ToString();
+                this.destination = dest.ToString();
+            }
+        }
+}

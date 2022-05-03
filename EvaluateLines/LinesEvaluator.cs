5f0a8196ed1d76b645a7a789bb87319cf06cbfdf
Yarin <yarin.greenfeld@gmail.com>
2022-05-03 23:17:06 +0300

    format LinesEvaluator

--- EvaluateLines/LinesEvaluator.cs
+++ EvaluateLines/LinesEvaluator.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Quixo
+﻿namespace Quixo
 {
     internal abstract class LinesEvaluator
     {
-        protected int UpdateContinuation(int currentContinuationValue) =>
-           
-			(currentContinuationValue ^ 2) * 4;
+        protected int UpdateContinuation(int currentContinuationValue) => (currentContinuationValue ^ 2) * 4;
         protected const int LosingLine = int.MinValue;
         protected const int WinningLine = int.MaxValue;
        abstract public int EvaluateLines(Board board, int evaluation);
